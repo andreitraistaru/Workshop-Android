64f927f894845adaaba068f096522c616a21bc78
andreitraistaru <56828222+andreitraistaru@users.noreply.github.com>
2020-08-15 23:18:27 +0300

    Elements added to SecondDescendantActivity.

--- app/src/main/java/com/workshopandroid/SecondDescendantActivity.java
+++ app/src/main/java/com/workshopandroid/SecondDescendantActivity.java
@@ -1,7 +1,9 @@
 package com.workshopandroid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SecondDescendantActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class SecondDescendantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_descendant);
+    }
+
+    public void goToFirstDescendantActivity(View view) {
+        startActivity(new Intent(this, FirstDescendantActivity.class));
     }
 }
