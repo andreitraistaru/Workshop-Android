c3f85d2278972ca60f6eff69766a0ee308b1e38b
andreitraistaru <56828222+andreitraistaru@users.noreply.github.com>
2020-08-15 22:54:41 +0300

    Navigation drawer added.

--- app/src/main/java/com/workshopandroid/MainActivity.java
+++ app/src/main/java/com/workshopandroid/MainActivity.java
@@ -1,21 +1,45 @@
 package com.workshopandroid;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_main_activity);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.drawer_toggle_open, R.string.drawer_toggle_close);
+        if (toggle != null) {
+            drawerLayout.addDrawerListener(toggle);
+        }
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }
     }
 
     public void saveButton(View view) {
@@ -74,5 +98,37 @@ public class MainActivity extends AppCompatActivity {
 
     public void actionButton(View view) {
         Snackbar.make(view, getResources().getString(R.string.snackbar_message), Snackbar.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+
+        switch (menuItem.getItemId()) {
+            case R.id.start_descendant1:
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                startActivity(new Intent(this, FirstDescendantActivity.class));
+
+                return true;
+            case R.id.start_descendant2:
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                startActivity(new Intent(this, SecondDescendantActivity.class));
+
+                return true;
+            case R.id.start_google_maps:
+                drawerLayout.closeDrawer(GravityCompat.START);
+
+                Uri gpsCoordinates = Uri.parse(getResources().getString(R.string.gps));
+                Intent intent = new Intent(Intent.ACTION_VIEW, gpsCoordinates);
+                intent.setPackage(getResources().getString(R.string.google_maps));
+
+                startActivity(intent);
+
+                return true;
+            default:
+                return false;
+        }
     }
 }
