b9350e9f9f5ca6052d2415d19ce42c37875cdc63
andreitraistaru <56828222+andreitraistaru@users.noreply.github.com>
2020-08-15 20:09:32 +0300

    Main activity built.

--- app/src/main/java/com/workshopandroid/MainActivity.java
+++ app/src/main/java/com/workshopandroid/MainActivity.java
@@ -1,7 +1,14 @@
 package com.workshopandroid;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,63 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void saveButton(View view) {
+        StringBuilder message = new StringBuilder();
+
+        RadioButton radioButton1 = findViewById(R.id.radioButton1_main_activity);
+        RadioButton radioButton2 = findViewById(R.id.radioButton2_main_activity);
+        RadioButton radioButton3 = findViewById(R.id.radioButton3_main_activity);
+
+        if (radioButton1.isChecked()) {
+            message.append(getResources().getString(R.string.radio_button1_main_activity));
+            message.append(" selected\n");
+        } else if (radioButton2.isChecked()) {
+            message.append(getResources().getString(R.string.radio_button2_main_activity));
+            message.append(" selected\n");
+        } else if (radioButton3.isChecked()) {
+            message.append(getResources().getString(R.string.radio_button3_main_activity));
+            message.append(" selected\n");
+        } else {
+            message.append("No radioButton selected!\n");
+        }
+
+        CheckBox checkBox1 = findViewById(R.id.checkBox1_main_activity);
+        CheckBox checkBox2 = findViewById(R.id.checkBox2_main_activity);
+
+        if (checkBox1.isChecked()) {
+            message.append(getResources().getString(R.string.checkbox1_main_activity));
+            message.append(" selected\n");
+        }
+
+        if (checkBox2.isChecked()) {
+            message.append(getResources().getString(R.string.checkbox2_main_activity));
+            message.append(" selected\n");
+        }
+
+        if (!checkBox1.isChecked() && !checkBox2.isChecked()) {
+            message.append("No checkBox selected!\n");
+        }
+
+        ToggleButton toggleButton = findViewById(R.id.toggleButton_main_activity);
+
+        if (toggleButton.isChecked()) {
+            message.append("ToggleButton on\n");
+        } else {
+            message.append("ToggleButton off\n");
+        }
+
+        EditText editText = findViewById(R.id.editText_main_activity);
+
+        message.append("Text is: ");
+        message.append(editText.getText());
+        message.append("\n");
+
+        Log.v("SAVE", message.toString());
+    }
+
+    public void actionButton(View view) {
+        Snackbar.make(view, getResources().getString(R.string.snackbar_message), Snackbar.LENGTH_LONG).show();
     }
 }
